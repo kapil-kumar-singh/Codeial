e1512560fedcd52fde225cf0002a0a089f3ef8cc
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-09-30 13:17:36 +0530

    cteate form for sign-in and sign out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,28 @@ module.exports.profile = function(req, res){
         title: "userProfile"
     });
 };
+
+
+// render the sign-in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Sign In"
+    })
+}
+
+// render the sign-up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Sign up"
+    })
+}
+
+// get the sign-up data
+module.exports.create = function(req, res){
+    // todo letter
+}
+
+// sign-in and create session for the user
+module.exports.createSession = function(req, res){
+    // todo letter
+}
