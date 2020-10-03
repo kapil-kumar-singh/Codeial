81b616dedc184e4d3aad1a26846d5baceabf033c
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-10-03 13:16:25 +0530

    showing the posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,18 @@
+const Post = require("../models/post");
+
 
 module.exports.home = function(req, res){
     console.log(req.cookies);
     // res.cookie('hbjh', 1);
-    return res.render('home', {
-        title: "Home"
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, post){
+        if(err){console.log('erroe in finding post',err); return;};
+        return res.render('home', {
+            title: 'Home | Codeial',
+            posts: post
+        });
     });
+
 };
 
