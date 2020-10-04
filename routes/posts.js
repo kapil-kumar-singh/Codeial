86b5d8a0538a1b2c0c0389280ca3eef3b32915bb
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-10-04 10:03:56 +0530

    Deleting a post (by Authorized user)

--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const passport = require('passport');
 const postController = require('../controllers/posts_controller');
 
 router.post('/create', passport.chechAuthentication, postController.create);
+router.get('/destroy/:id', passport.chechAuthentication, postController.destroy);
 
 module.exports = router;
