d7923584cd06ad49b0283aa0b0985469ca0142b6
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-10-05 11:20:16 +0530

    Creating Flash Messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -66,10 +66,12 @@ module.exports.create = function(req, res){
 
 // sign-in and create session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'You Have Logged Out!');
     return res.redirect('/');
 }
