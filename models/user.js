fc7ea132bbbb50f6eb823ca20a63b2f2880501a1
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-10-06 16:48:41 +0530

    Configuring Multer for User Avatar

--- models/user.js
+++ models/user.js
@@ -16,10 +16,25 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+    },
+    avatar: {
+        type: String
     }
 },{
     timestamps: true
 }); 
+
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname,'..', AVATAR_PATH));
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now());
+    }
+});
+
+
 
 const User = mongoose.model('User', userSchema);
 
