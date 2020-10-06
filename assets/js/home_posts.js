e2bc69db9d8113db91e44d0eafb3221fd7bbc381
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-10-06 15:45:50 +0530

    create and delete comments by ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -15,6 +15,20 @@
                     let newPost = newPostDom(data.data.post);
                     $('#post-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost))
+
+                    // call the create comment class
+                    console.log(data.data.post._id);
+                    new PostComments(data.data.post._id);
+
+                    new Noty({
+                      theme: 'relax',
+                      text: "Post published!",
+                      type: 'success',
+                      layout: 'topRight',
+                      timeout: 1500
+                      
+                  }).show();
+
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -36,7 +50,7 @@
           </small>
         </p>
         <div class="post-comments">
-            <form action="/comments/create" method="post">
+            <form id="post-${post._id}-comments-form" action="/comments/create" method="post">
               <input type="text" name="content" placeholder="Type Here to add comment..." required>
               <input type="hidden" name="post" value="${ post._id }">
               <input type="submit" value="Add Comment">
@@ -60,12 +74,35 @@
           success: function(data){
             console.log(data);
             $(`#post-${data.data.post_id}`).remove();
+            new Noty({
+              theme: 'relax',
+              text: "Post Deleted",
+              type: 'success',
+              layout: 'topRight',
+              timeout: 1500
+            }).show();
           }, errror: function(error){
             console.log(error.responseText);
           }
         })
-      })    }
+      })    
+    }
+
+    // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+
+    let convertPostsToAjax = function(){
+      $('#post-list-container>ul>li').each(function(){
+        let self = $(this);
+        let deleteButton = $(' .delete-post-button', self);
+        deletePost(deleteButton);
+
+        // get the post's id by splitting the id attribute
+        let postId = self.prop('id').split('-')[1]
+        new PostComments(postId);
+      });
+    }
 
 
     createPost();
+    convertPostsToAjax();
 }
--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -32,6 +32,19 @@ module.exports.create = async function(req, res){
 
                 post.comments.push(comment);
                 post.save();
+
+                if (req.xhr){
+                    // Similar for comments to fetch the user's id!
+                    comment = await comment.populate('user', 'name').execPopulate();
+        
+                    return res.status(200).json({
+                        data: {
+                            comment: comment
+                        },
+                        message: "Post created!"
+                    });
+                }
+
                 req.flash('success', 'Comment Published!')
                 return res.redirect('/');
         };
@@ -70,6 +83,17 @@ module.exports.destroy = async function(req, res){
 
                 comment.remove();
                 let post = Post.findByIdAndUpdate(postId, { $pull : { comments : req.params.id}});
+                
+                    // send the comment id which was deleted back to the views
+                     if (req.xhr){
+                        return res.status(200).json({
+                            data: {
+                                comment_id: req.params.id
+                            },
+                            message: "Commet Deleted"
+                        });
+                    }
+
                     req.flash('success', 'Comment deleted')
                     return res.redirect('back');
             }else{
