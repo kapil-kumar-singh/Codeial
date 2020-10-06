70f7ac82eb0231d5469a9434fbb3dd88f1ebe64f
kapil-kumar-singh <kapilkumarsingh44@gmail.com>
2020-10-06 08:40:11 +0530

    Creating a Post :: Sending Data and Receiving Data

---
+++ assets/js/home_posts.js
@@ -0,0 +1,52 @@
+{
+
+    // method to submit the form data for a new post using Ajax
+    let createPost = function(){
+        let newPostForm = $('#new-post-form');
+        newPostForm.submit(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+                    // console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#post-list-container>ul').prepend(newPost);
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        });
+    };
+
+    // method to create post in DOM
+    let newPostDom = function(post){
+        return $(`<li id="post-${ post._id }">
+        <p>
+            <small>
+              <a class="delete-post-button" href='/posts/destroy/<%=post.id%>'>X</a>
+            </small>
+            ${post.content}
+          <br>
+          <small>
+          ${ post.user.name}
+          </small>
+        </p>
+        <div class="post-comments">
+            <form action="/comments/create" method="post">
+              <input type="text" name="content" placeholder="Type Here to add comment..." required>
+              <input type="hidden" name="post" value="${ post._id }">
+              <input type="submit" value="Add Comment">
+            </form>
+          <div id='post-comment-list'>
+            <ul id='post-comment-${post._id}'>
+            </ul>
+          </div>
+        </div>
+      </li>`)
+    }
+
+    createPost();
+}
